34249b36a5a7a2f78754692432e7685fe4e44e3e
fnhnafiz <nafsinsifan2k21@gmail.com>
2025-06-30 20:00:18 +0600

    connect add event api with frontend site

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
+const { MongoClient, ServerApiVersion } = require("mongodb");
 const express = require("express");
 const cors = require("cors");
 const app = express();
-
-const { MongoClient, ServerApiVersion } = require("mongodb");
 
 require("dotenv").config();
 
@@ -10,7 +9,14 @@ const port = process.env.PORT || 5000;
 
 app.use(express.json());
 
-const uri = `mongodb+srv://${process.env.USERNAME}:${process.env.PASSWORD}@cluster0.udh1k.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
+const corsOptions = {
+  origin: ["http://localhost:5173"],
+  credentials: true,
+  optionSuccessStatus: 200,
+};
+
+app.use(cors(corsOptions));
+const uri = `mongodb+srv://${process.env.Events_USERS}:${process.env.Events_PASS}@cluster0.udh1k.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -33,11 +39,28 @@ async function run() {
       const result = await allUsers.insertOne(user);
       res.send(result);
     });
+    // app.get("/user", async (req,res)=>{
+    //     const user = await
+    // })
+
+    //when a user add event form submit the user event information stored in the luxEvent database.
+    app.post("/add-event", async (req, res) => {
+      const event = req.body;
+      const result = await allEvent.insertOne(event);
+      res.send(result);
+    });
+
+    //show
+    app.get("/all-event", async (req, res) => {
+      const result = await allEvent.find().toArray();
+      res.send(result);
+    });
   } finally {
   }
 }
 
 run().catch(console.dir);
+
 app.get("/", (req, res) => {
   res.send("job-task is running");
 });
