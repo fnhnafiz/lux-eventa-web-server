01f6f525a2ad6c987bd48780ac355d7ccb213c07
fnhnafiz <nafsinsifan2k21@gmail.com>
2025-07-01 20:28:54 +0600

    server was problem

--- index.js
+++ index.js
@@ -316,6 +316,3 @@ run().catch(console.dir);
 app.get("/", (req, res) => {
   res.send(`LuxEventa Server is running on port ${port}`);
 });
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
-});
